720611a47a45af824ad4fc091e2b794e59ebf5cf
ramon.l.echeverria <ramon.l.echeverria@gmail.com>
2020-03-25 00:27:49 +0000

    Done.

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,7 +1,7 @@
-
+require 'pry'
 
 def get_user_input
-  gets.chomp
+ num = gets.chomp
 end
 
 def prompt_user
@@ -11,14 +11,15 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
-     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-     "HAM HAM HAM IN MY TUMMY"
+  if num == 1
+    return "YUM YUM MUNCH MUNCH MUNCH"
+  elsif num == 2
+    return "HAM HAM HAM IN MY TUMMY"
   end
 end
 
 def runner
   prompt_user
-  selection(get_user_input)
+  get_user_input
+  selection(num)
 end
--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,6 @@
 def snake_it_up(string)
-  if string[0] == "s"
-  10 * "s" + string
-  else
-  string
+  10.times do
+    string = "s" + string
   end
+  string 
 end
